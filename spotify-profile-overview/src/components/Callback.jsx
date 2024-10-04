b8d621636190bccd8fd115056b1e93709c368d28
Andy Roh <rohjamesandy@gmail.com>
2024-10-04 05:27:07 -0400

    Struggling getting the Login to work...

--- spotify-profile-overview/src/components/Callback.jsx
+++ spotify-profile-overview/src/components/Callback.jsx
@@ -9,31 +9,27 @@ export default function Callback() {
   useEffect(() => {
     const hash = window.location.hash;
     let token = window.localStorage.getItem('token');
-
+  
     // Check if the token exists in localStorage or in the URL hash
     if (!token && hash) {
       token = new URLSearchParams(hash.substring(1)).get('access_token');
       
       if (token) {
-        // Store the token in localStorage and update the state
         window.localStorage.setItem('token', token);
         setToken(token);
-        // Redirect to the profile page
-        navigate('/');
+        navigate('/');  // Redirect to the profile page
       } else {
-        // If no token is found, redirect back to the login page
         console.error('No token found in URL');
         navigate('/');
       }
     } else if (token) {
-      // If the token exists in localStorage, use it
       setToken(token);
       navigate('/');
     } else {
-      // If no token is found in both localStorage and URL hash, redirect to login
       navigate('/');
     }
   }, [setToken, navigate]);
+  
 
   return <div>Logging in...</div>;
 }

3764eb740dfda18af3da99203409cb02ed44bac9
Andy Roh <rohjamesandy@gmail.com>
2024-10-04 04:52:19 -0400

    Updated the AuthProvider to match the HashRouter

--- spotify-profile-overview/src/components/AuthProvider.jsx
+++ spotify-profile-overview/src/components/AuthProvider.jsx
@@ -16,7 +16,7 @@ export function AuthProvider({ children }) {
 
   const login = () => {
     const CLIENT_ID = '3dbc0c6e8950478687663a14bae7adea';  // Your actual Client ID
-    const REDIRECT_URI = 'https://andyrohdev.github.io/spotify-profile-overview/callback';  // GitHub Pages URL
+    const REDIRECT_URI = 'https://andyrohdev.github.io/spotify-profile-overview/#/callback';  // GitHub Pages URL with hash
     const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
     const RESPONSE_TYPE = 'token';
     const SCOPES = 'user-read-private user-read-email';
